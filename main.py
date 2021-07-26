36df2930910dba00e950928f632dc942cbfc8f65
crocodeev <svin862003@mail.ru>
2021-07-26 13:29:06 +0300

    add kill process

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from job import isprocessrunning
+from task import isprocessrunning, killProcess
 
 import win32serviceutil
 import win32service
@@ -7,16 +7,18 @@ import servicemanager
 
 
 import socket
-import logging
 import sys
 import time
 
-logging.basicConfig(filename="service.log", level=logging.DEBUG)
+from logg import logg
 
-firstProcessName = 'in-co-test.exe'
-firstProcessPath = 'E:\LINKS\data_chizzza_ss\inplay-control\in-co-test.exe'
-secondProcessName = 'in-co-supp.exe'
-secondProcessPath = 'E:\LINKS\data_chizzza_ss\inplay-support\in-co-supp.exe'
+processArray2 = [('in-co-test.exe', 'E:\\LINKS\\data_chizzza_ss\\inplay-control\\in-co-test.exe'),
+                 ('in-co-supp.exe', 'E:\\LINKS\\data_chizzza_ss\\inplay-support\\in-co-supp.exe'),
+                 ('mediaroom3.exe', 'E:\LINKS\data_chizzza_ss\mediaroom3.exe')]
+
+processArray = [('testWebServer.exe', 'C:\\incoWatchDog\\testWebServer.exe')]
+
+
 
 class WatchDogService(win32serviceutil.ServiceFramework):
     _svc_name_ = "incoWatchDog"
@@ -29,23 +31,34 @@ class WatchDogService(win32serviceutil.ServiceFramework):
         socket.setdefaulttimeout(60)
 
     def SvcDoRun(self):
+        logg("SERVICE IS STARTING...")
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
                               servicemanager.PYS_SERVICE_STARTED,
                               (self._svc_name_, ''))
         self.main()
 
     def SvcStop(self):
+        logg("SERVICE CALLED TO STOP")
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         win32event.SetEvent(self.hWaitStop)
+        self.stop()
+
+    def stop(self):
+        logg("START KILLING PROCESSES")
+        for process in processArray:
+            try:
+                killProcess(process[0])
+            except Exception as e:
+                logg("Error when stop " + process[0])
 
     def main(self):
 
         """DO THE RUN STUFF HERE"""
-        logging.info("SERVICE MAIN ENTERED !!")
+        logg("SERVICE MAIN ENTERED !!")
         rc = None
         while rc != win32event.WAIT_OBJECT_0:
-            isprocessrunning(firstProcessName, firstProcessPath)
-            isprocessrunning(secondProcessName, secondProcessPath)
+            for process in processArray:
+                isprocessrunning(process[0], process[1])
             rc = win32event.WaitForSingleObject(self.hWaitStop, 10000)
             time.sleep(10)
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
--- task.py
+++ task.py
@@ -4,12 +4,21 @@ import psutil
 import subprocess
 from logg import logg
 
+def killProcess(processName):
+    for process in psutil.process_iter():
+        try:
+            if processName.lower() in process.name().lower():
+                process.kill()
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
 
 
 def startProcess(processPath):
     try:
         logg('TRYING TO START PROCESS')
-        subprocess.Popen(processPath, shell=True)
+        logg(processPath)
+        proc = subprocess.Popen(processPath, shell=False)
     except Exception as e:
         logg(e)
 
@@ -24,6 +33,6 @@ def isprocessrunning(processName, processPath):
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
     if isExist == False:
-        logg(processName + ' doesn\'t exist')
+        logg(processName + 'process doesn\'t run')
         startProcess(processPath)
         isExist = False
